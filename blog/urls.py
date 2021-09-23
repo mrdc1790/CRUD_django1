47308b7ba336fd2713e479e73dc0d8401d35c2e6
apollac4 <61246494+mrdc1790@users.noreply.github.com>
2021-09-23 12:43:32 -0400

    applications and routes
    started url routes

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='about-home'),
 ]
